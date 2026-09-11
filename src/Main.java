1693fd47428cb42ba6c82f9c35179afc79f14df8
arpit@5220 <arpitkatiyar09197@gmail.com>
2026-09-12 02:02:00 +0530

    commit first

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // fun(List.of(1, 2, 3, 4, 5, 6,6,7,8));
+
+        List<String> courses = List.of("Spring", "Spring Boot", "API" , "Microservices","AWS", "PCF","Azure", "Docker", "Kubernetes");
+
+        courses.stream()
+                // .filter(course -> course.length()>=4)
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);
+    }
+
+    public static void fun(List<Integer> numbers) {
+//        for(int number:numbers)
+//        {
+//            System.out.println(number);
+//        }
+
+        numbers.stream()
+                //.filter(Main::isEven) // only allows even elements
+                .filter(number -> number%2 ==0)  // Lambda Expression
+                .forEach(Main::print); // Method Reference
+    }
+
+    private static void print(int number)
+    {
+        System.out.println(number);
+    }
+
+    private static boolean isEven(int number)
+    {
+        return number%2 == 0;
     }
 }
---
+++ src/Programming01/FP01Structured.java
@@ -0,0 +1,10 @@
+package Programming01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FP01Structured {
+
+
+
+}
